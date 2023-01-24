a3d16e8fff69d355e7a2e7372ee4a000cbf46f61
lvovgit <romka.lvov@gmail.com>
2023-01-24 21:44:40 +0300

    add func first_letter_up

--- str_func.py
+++ str_func.py
@@ -3,4 +3,11 @@ def upper():
     print(f'{s.upper()}')
     return s
 
+def first_letter_up():
+    i = input("Ваша строка")
+    capitalized_i = i.capitalize()
+    print(f'{capitalized_i}')
+    return print()
+
 upper()
+first_letter_up()
